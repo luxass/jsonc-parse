e90a0afa0b23632d944222fed3ea1772474a3087
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-12-26 14:41:18 +0100

    added type module to pkg

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["./src/index.ts"],
+  format: ["cjs", "esm"],
   dts: true,
   clean: true,
-  treeshake: true,
-  format: ["esm", "cjs"]
+  treeshake: true
 });
