ec8f239723d7c64791bf4a259d20e7fd715ece03
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-06-30 08:36:12 +0200

    chore: lint

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,52 +1,52 @@
-import { describe, expect, it } from "vitest"
-import { parse, parseFile, parseFileSync } from "../src"
+import { describe, expect, it } from "vitest";
+import { parse, parseFile, parseFileSync } from "../src";
 
 describe("jsonc-parse", () => {
   it("parse jsonc from file (async)", async () => {
-    const test1 = await parseFile("./tests/test1.jsonc")
-    const test2 = await parseFile("./tests/test2.jsonc")
+    const test1 = await parseFile("./tests/test1.jsonc");
+    const test2 = await parseFile("./tests/test2.jsonc");
 
     expect(test1?.bb).toEqual([
       1,
       "Hey!",
-    ])
-    expect(test1?.num).toBe(1)
+    ]);
+    expect(test1?.num).toBe(1);
 
-    expect(test2?.aa).toBeTypeOf("object")
+    expect(test2?.aa).toBeTypeOf("object");
     expect(test2?.nested).toEqual({
       this: {
         is: {
           a: "nested",
         },
       },
-    })
+    });
 
-    expect(test1?.bar).toEqual(test2?.bar)
-    expect(test1?.foo).toEqual(test2?.foo)
-  })
+    expect(test1?.bar).toEqual(test2?.bar);
+    expect(test1?.foo).toEqual(test2?.foo);
+  });
 
   it("parse jsonc from file", () => {
-    const test1 = parseFileSync("./tests/test1.jsonc")
-    const test2 = parseFileSync("./tests/test2.jsonc")
+    const test1 = parseFileSync("./tests/test1.jsonc");
+    const test2 = parseFileSync("./tests/test2.jsonc");
 
     expect(test1?.bb).toEqual([
       1,
       "Hey!",
-    ])
-    expect(test1?.num).toBe(1)
+    ]);
+    expect(test1?.num).toBe(1);
 
-    expect(test2?.aa).toBeTypeOf("object")
+    expect(test2?.aa).toBeTypeOf("object");
     expect(test2?.nested).toEqual({
       this: {
         is: {
           a: "nested",
         },
       },
-    })
+    });
 
-    expect(test1?.bar).toEqual(test2?.bar)
-    expect(test1?.foo).toEqual(test2?.foo)
-  })
+    expect(test1?.bar).toEqual(test2?.bar);
+    expect(test1?.foo).toEqual(test2?.foo);
+  });
 
   it("parse jsonc from string", () => {
     const jsonC = parse(`
@@ -56,10 +56,10 @@ describe("jsonc-parse", () => {
         "foo": /* This is also a comment */ "bar",
 
       }
-    `)
+    `);
     expect(jsonC).toEqual({
       bar: "foo",
       foo: "bar",
-    })
-  })
-})
+    });
+  });
+});
